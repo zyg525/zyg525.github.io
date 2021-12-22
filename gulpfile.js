477ea8d43cb8b3315ed16dfa5e887fe502604d14
zhonger <zhonger@live.cn>
2021-12-22 14:32:14 +0900

    Optimize the automatic workflow for static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
 var gulp = require('gulp'),
+    concat = require('gulp-concat'), //合并css文件
     uglify = require('gulp-uglify'), // 压缩js文件
-    sass = require('gulp-sass'), // 编译sass
+    sass = require('gulp-sass')(require('node-sass')), // 编译sass
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(){
+gulp.task('scripts', (cb) => {
     gulp.src('dev/js/index.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js'))
+        .pipe(gulp.dest('assets/js'));
+    cb();
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', (cb) => {
     gulp.src('dev/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('dev/sass'))
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'));
+    cb();
 });
 
-gulp.task('watch', function(){
-    gulp.watch('dev/sass/*.scss', ['sass']);
-    gulp.watch('dev/js/*.js', ['scripts']);
+gulp.task('css', (cb) => {
+    gulp.src(['dev/sass/prism.css', 'dev/sass/github-markdown.css', 'dev/sass/share.min.css'])
+        .pipe(concat('plugins.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('assets/css'));
+    cb();
 });
 
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch('dev/sass/*.scss', gulp.series('sass'));
+    gulp.watch('dev/sass/*.css', gulp.series("css"));
+    gulp.watch('dev/js/*.js', gulp.series("scripts"));
+});
+
+gulp.task('default', gulp.series('scripts', 'sass', 'css', 'watch'));
